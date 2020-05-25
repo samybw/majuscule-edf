93d19455c3a59ae9a184f0e8511bcbbcff41f55a
samy <samybn@yahoo.com>
2020-05-25 11:37:21 +0200

    Il me manque plus qu'a trouve un moyen de tout mettre sur 1 ligne

---
+++ majuscule.js
@@ -0,0 +1,9 @@
+var phrase = "bien le bonjour";
+for (let i = 0; i < phrase.length; i++){
+    if ( i % 2 == 0 ){
+        console.log(phrase[i].toLowerCase());
+    }else
+     console.log(phrase[i].toUpperCase());
+     
+}
+
