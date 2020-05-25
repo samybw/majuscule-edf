50e80f8aae43b163c4bb9b862ef84dfd87666ffb
samy <samybn@yahoo.com>
2020-05-25 15:10:54 +0200

    j'ai ajoute une variable resultat

--- majuscule.js
+++ majuscule.js
@@ -1,9 +1,45 @@
-var phrase = "bien le bonjour";
-for (let i = 0; i < phrase.length; i++){
+
+var phrase  = process.argv[2];
+var i = 0
+resultat = "";
+while (i < phrase.length) {
+    if ( i % 2 == 0 ){
+       resultat += phrase[i].toLowerCase();
+    }else{ 
+     resultat += phrase[i].toUpperCase();
+    }
+    i++
+}
+
+console.log(resultat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for (let i = 0; i < phrase.length; i++){
     if ( i % 2 == 0 ){
         console.log(phrase[i].toLowerCase());
-    }else
+    }else{ 
      console.log(phrase[i].toUpperCase());
-     
-}
+    }
+}*/
+/*var i = 0
+while (i < phrase.length) {
+    if ( i % 2 == 0 ){
+        console.log(phrase[i].toLowerCase());
+    }else{ 
+     console.log(phrase[i].toUpperCase());
+    }
+    i++
+}*/
+
 
